a2d477e929849f4daa4cf3411335832f9390cb77
Bruce Richardson <it.is.bruce@gmail.com>
2015-01-20 09:14:04 +0000

    Added methods which need pattern matching

--- part4/option.scala
+++ part4/option.scala
@@ -1,7 +1,14 @@
+// Exercise 4.1
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B]
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case _ => None
+  }
   def flatMap[B](f: A => Option[B]): Option[B]
-  def getOrElse[B >: A](default: => B): B
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case _ => default
+  }
   def orElse[B >:A](ob: => Option[B]): Option[B]
   def filter(f: A => Boolean): Option[A]
 }
