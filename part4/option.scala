5de9fee4f26bc1cd112a02422da487f5d7707296
Bruce Richardson <it.is.bruce@gmail.com>
2015-01-20 08:34:35 +0000

    Um, with the rest of the template

--- part4/option.scala
+++ part4/option.scala
@@ -1,7 +1,11 @@
-trait Option[+A] {
+sealed trait Option[+A] {
   def map[B](f: A => B): Option[B]
   def flatMap[B](f: A => Option[B]): Option[B]
   def getOrElse[B >: A](default: => B): B
   def orElse[B >:A](ob: => Option[B]): Option[B]
   def filter(f: A => Boolean): Option[A]
 }
+case class Some[+A](get: A) extends Option[A]
+// Exercise work
+case object None extends Option[Nothing]
+// Exercise work
