02061e4f364d7366e90f0f015fc8a0cdf0668c60
Bruce Richardson <it.is.bruce@gmail.com>
2015-08-23 21:20:01 +0100

    Spacing

--- part4/option.scala
+++ part4/option.scala
@@ -4,13 +4,17 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
     case _ => None
   }
+
   def flatMap[B](f: A => Option[B]): Option[B] = this map f getOrElse None
+
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(a) => a
     case _ => default
   }
+
   def orElse[B >:A](ob: => Option[B]): Option[B] =
     this map (Some(_)) getOrElse ob
+
   def filter(f: A => Boolean): Option[A] =
     /*I did filter before flatMap
     if (this map f getOrElse false) this else None
