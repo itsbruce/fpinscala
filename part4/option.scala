af36e2860123f67a0f94fa394e8524c2dfd8bf5a
Bruce Richardson <it.is.bruce@gmail.com>
2015-08-24 23:54:21 +0100

    Fixed some Option code I must have written while *very* tired

--- part4/option.scala
+++ part4/option.scala
@@ -1,19 +1,24 @@
 // Exercise 4.1
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
+    case None => None
     case Some(a) => Some(f(a))
-    case _ => None
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = this map f getOrElse None
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
 
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(a) => a
     case _ => default
   }
 
-  def orElse[B >:A](ob: => Option[B]): Option[B] =
-    this map (Some(_)) getOrElse ob
+  def orElse[B >:A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
 
   def filter(f: A => Boolean): Option[A] =
     /*I did filter before flatMap
