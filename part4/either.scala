70bda087726ae58ee348b826a4039a4f303c233d
Bruce Richardson <it.is.bruce@gmail.com>
2015-08-26 11:22:45 +0100

    Either example functions added

--- part4/either.scala
+++ part4/either.scala
@@ -3,3 +3,21 @@
 sealed trait Either[+E, +A]
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  // Adding example code
+  def mean(xs:IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+
+}
