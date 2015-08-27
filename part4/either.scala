7a0fc977cfad0e4f2a288c70048559c9e3e01ed6
Bruce Richardson <it.is.bruce@gmail.com>
2015-08-27 10:36:22 +0100

    Completed 4.6

--- part4/either.scala
+++ part4/either.scala
@@ -1,6 +1,31 @@
 // Chapter 4 - either
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  // 4.6
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C) =
+    this flatMap { a => b map { f(a, _) } }
+
+  /* map2 using for expression
+  def map2for[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C) =
+    for { a <- this; bb <- b } yield f(a, bb)
+   */
+
+}
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
