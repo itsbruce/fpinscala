8ef53f41f033dafc4144e55e3de11b6a101300a3
Bruce Richardson <it.is.bruce@gmail.com>
2015-09-01 17:00:31 +0100

    6.9 completed

--- part6/state.scala
+++ part6/state.scala
@@ -111,4 +111,11 @@ object RNG {
       else nonNegativeLessThan(n)
     }
 
+  // 6.9
+  def mapF[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2F[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra){a => mapF(rb)(b => f(a, b))}
+
 }
